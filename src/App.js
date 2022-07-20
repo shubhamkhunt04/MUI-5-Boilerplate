4ddf1563cfd0a6f5f7b62bb916e1e46a32c237d9
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2022-07-20 19:25:58 +0530

    Button component completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,91 @@
+// * React Imports
 import React from 'react';
+
+// * Mui Imports
 import { Box } from '@mui/material';
+
+// * Components Imports
 import Button from './Components/Button';
 
 const App = () => {
   return (
     <Box bgcolor='#140838' height='100vh' p={10}>
-      <Box display='flex' justifyContent='center' gap={2} m={10}>
+      {/* Large Buttons */}
+      <Box
+        display='flex'
+        justifyContent='center'
+        gap={2}
+        m={10}
+        alignItems='center'
+      >
         <Button variant='contained' size='large'>
           Next Step
         </Button>
-        <Button variant='contained' size='medium'>
+        <Button variant='contained' color='secondary' size='large'>
           Next Step
         </Button>
-        <Button variant='contained' size='small'>
+        <Button variant='outlined' size='large'>
+          Leave
+        </Button>
+        <Button variant='outlined' color='secondary' size='large'>
+          Leave
+        </Button>
+        <Button variant='contained' size='large' disabled>
+          Next Step
+        </Button>
+        <Button variant='contained' color='secondary' size='large' disabled>
           Next Step
         </Button>
       </Box>
 
-      <Box display='flex' justifyContent='center' gap={2} m={10}>
-        <Button variant='outlined' size='large'>
+      {/* Medium Buttons */}
+      <Box
+        display='flex'
+        justifyContent='center'
+        gap={2}
+        m={10}
+        alignItems='center'
+      >
+        <Button variant='contained'>Next Step</Button>
+        <Button variant='contained' color='secondary'>
           Next Step
         </Button>
-        <Button variant='outlined' size='medium'>
+        <Button variant='outlined'>Leave</Button>
+        <Button variant='outlined' color='secondary'>
+          Leave
+        </Button>
+        <Button variant='contained' disabled>
+          Next Step
+        </Button>
+        <Button variant='contained' color='secondary' disabled>
+          Next Step
+        </Button>
+      </Box>
+
+      {/* Small Buttons */}
+      <Box
+        display='flex'
+        justifyContent='center'
+        gap={2}
+        m={10}
+        alignItems='center'
+      >
+        <Button variant='contained' size='small'>
+          Next Step
+        </Button>
+        <Button variant='contained' color='secondary' size='small'>
           Next Step
         </Button>
         <Button variant='outlined' size='small'>
+          Leave
+        </Button>
+        <Button variant='outlined' color='secondary' size='small'>
+          Leave
+        </Button>
+        <Button variant='contained' size='small' disabled>
+          Next Step
+        </Button>
+        <Button variant='contained' color='secondary' size='small' disabled>
           Next Step
         </Button>
       </Box>
